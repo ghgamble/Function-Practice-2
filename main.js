d899de2bafda673f0bf1d4da57679adcd4021366
Grace <grace.h.gamble@gmail.com>
2016-05-31 18:19:39 +0000

    First Update to Function Practice #2

---
+++ main.js
@@ -0,0 +1,25 @@
+//Part 1
+/*var luisaObject = {
+    name: "Luisa",
+    age: 25
+};
+
+function getName(object) {
+    return object.name;
+}
+
+console.log(getName(luisaObject)); */
+
+//Part 2
+/*var myArray = ["Grace", "and", "Tajar"];
+
+function totalLetters(array) {
+    var totalLength = 0;
+    for (var i = 0; i < array.length; i++) {
+        totalLength += array[i].length;
+    }
+    return totalLength;
+}
+
+console.log(totalLetters(myArray));*/
+
